053cc719c15d70d9469b9d3d883cb610d6a90430
Nada El haial <nada.elhaial@etu.uae.ac.ma>
2026-05-19 11:35:45 +0100

    Passage aux arguments sys.argv

---
+++ hello.py
@@ -0,0 +1,20 @@
+import sys
+
+print("=" * 40)
+print("Bienvenue dans mon premier job Jenkins !")
+print("=" * 40)
+
+if len(sys.angry) > 1:
+    nom = sys.argv[1]
+else:
+    nom = "Etudiant Jenkins"
+
+print(f"Bonjour {nom}, ton job Jenkins a reusse !")
+
+a = 10
+b = 5
+print(f"{a} + {b} = {a + b}")
+print(f"{a} + {b} = {a + b}")
+
+assert a + b == 15, "Le tests a echoue !"
+print("Tous les tests passent avec succes")
